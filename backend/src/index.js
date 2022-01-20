35fa39acac53612ef7f15ff6b7730a8bf03036ed
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2022-01-20 22:58:55 +0500

    minor changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,35 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const env = require('dotenv')
+const userRoutes = require('./routes/user')
 
 env.config();
 
 const app = express()
 
 app.use(express.json()) //body parser
+app.use('/api', userRoutes)
 
-mongoose.connect(process.env.DB_URL);
-
-const Cat = mongoose.model('Cat', {
-    name: String
-});
-
-const kitty = new Cat({
-    name: 'Zildjian'
-});
-kitty.save().then(() => console.log('meow'));
-
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Hello from server'
-    })
-})
-
-app.post('/data', (req, res, next) => {
-    res.status(200).json({
-        message: req.body
-    })
-})
+mongoose.connect(process.env.DB_URL).then(
+    console.log('connected to database')
+);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
